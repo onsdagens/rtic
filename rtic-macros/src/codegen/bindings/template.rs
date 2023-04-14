e7ffa50ec24f0292afa0b31811efda494e9b7a3a
onsdagens <pawdzi-7@student.ltu.se>
2023-04-14 12:25:56 +0200

    Added ESP32C3 support

--- rtic-macros/src/codegen/bindings/template.rs
+++ rtic-macros/src/codegen/bindings/template.rs
@@ -46,3 +46,10 @@ pub fn interrupt_exit(_app: &App, _analysis: &CodegenAnalysis) -> Vec<TokenStrea
 pub fn async_prio_limit(_app: &App, _analysis: &CodegenAnalysis) -> Vec<TokenStream2> {
     vec![]
 }
+pub fn async_entry(_app: &App, _analysis: &CodegenAnalysis, _dispatcher_name: Ident) -> Vec<TokenStream2>{
+    vec![]
+}
+
+pub fn async_prio_limit(app: &App, _analysis: &CodegenAnalysis) -> Vec<TokenStream2> {
+    vec![]
+}
